332e775837bb61b18ea83f83afeac3609c877966
jchilala50-blip <jchilala50@gmail.com>
2026-05-15 00:08:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,28 +2,27 @@ const WebSocket = require('ws');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Bot Online'));
+app.get('/', (req, res) => res.send('Bot Ativo'));
 app.listen(process.env.PORT || 3000);
 
 const ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');
 
 ws.on('open', () => {
-    console.log('Conectado à Deriv!');
-    const token = process.env.DERIV_TOKEN;
-    ws.send(JSON.stringify({ authorize: token }));
+    console.log('Conectado! Autenticando...');
+    ws.send(JSON.stringify({ authorize: process.env.DERIV_TOKEN }));
 });
 
 ws.on('message', (data) => {
     const msg = JSON.parse(data);
     if (msg.msg_type === 'authorize') {
         if (msg.error) {
-            console.log('Erro no Token:', msg.error.message);
+            console.log('Erro de Token:', msg.error.message);
         } else {
-            console.log('Login Sucesso! Email:', msg.authorize.email);
+            console.log('Sucesso! Logado como:', msg.authorize.email);
             ws.send(JSON.stringify({ balance: 1, subscribe: 1 }));
         }
     }
     if (msg.msg_type === 'balance') {
-        console.log('SALDO:', msg.balance.currency, msg.balance.balance);
+        console.log('SALDO ATUAL:', msg.balance.balance);
     }
 });
