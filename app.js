4e5828df1520e7f8153ce74455d88f8968401c85
jchilala50-blip <jchilala50@gmail.com>
2026-05-15 06:33:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-const WebSocket = require('ws');
 const express = require('express');
+const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Bot Operacional'));
-app.listen(process.env.PORT || 3000);
+// Serve os ficheiros da pasta atual
+app.use(express.static(__dirname));
 
-const ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');
-
-ws.on('open', () => {
-    console.log('CONECTADO! Validando o seu token...');
-    ws.send(JSON.stringify({ authorize: process.env.DERIV_TOKEN }));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-ws.on('message', (data) => {
-    const msg = JSON.parse(data);
-    if (msg.msg_type === 'authorize') {
-        if (msg.error) {
-            console.log('ERRO DE TOKEN:', msg.error.message);
-        } else {
-            console.log('LOGIN ACEITO! Bem-vindo:', msg.authorize.email);
-            ws.send(JSON.stringify({ balance: 1, subscribe: 1 }));
-        }
-    }
-    if (msg.msg_type === 'balance') {
-        console.log('O SEU SALDO É:', msg.balance.balance);
-    }
-});
+app.listen(port, () => console.log(`Site online na porta ${port}`));
