b6ff153be5e4a1073051d1fefce721aeee90b274
jchilala50-blip <jchilala50@gmail.com>
2026-05-15 08:52:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve os ficheiros da pasta atual
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => console.log(`Site online na porta ${port}`));
+app.listen(port, () => {
+    console.log(`Kubiko Trader rodando na porta ${port}`);
+});
